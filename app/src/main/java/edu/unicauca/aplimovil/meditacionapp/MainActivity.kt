311e76296c1e2dabd463bccb41972b33f402b547
cfigmart <cfigmart@unicauca.edu.co>
2025-03-12 17:56:46 -0500

    Initial commit

---
+++ app/src/main/java/edu/unicauca/aplimovil/meditacionapp/MainActivity.kt
@@ -0,0 +1,92 @@
+package edu.unicauca.aplimovil.meditacionapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import edu.unicauca.aplimovil.meditacionapp.ui.theme.MeditacionAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MeditacionAppTheme {
+
+            }
+        }
+    }
+}
+
+@Composable
+fun BarraDeBusqueda(
+    modifier: Modifier = Modifier
+){
+    TextField(
+        value = "",
+        onValueChange = {},
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = null
+            )
+        },
+        placeholder = {
+            Text(text= stringResource(R.string.search_txt))
+        },
+        colors = TextFieldDefaults.colors(
+            unfocusedContainerColor = MaterialTheme.colorScheme.surface,
+            focusedContainerColor = MaterialTheme.colorScheme.surface
+        ),
+        modifier = modifier
+            .fillMaxWidth()
+            .heightIn(min = 56.dp)
+    )
+}
+
+@Preview
+@Composable
+fun PreviewBarraDeBusqueda() {
+    BarraDeBusqueda()
+}
+
+@Composable
+fun CuadroAlineaTuCuerpo(){
+    Column {
+        Image(
+            painter = painterResource(R.drawable.ab1_inversions),
+            contentDescription = stringResource(R.string.ab1_inversions_txt)
+        )
+        Text(
+            text = stringResource(R.string.ab1_inversions_txt)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun PreviewCuadroAlineaTuCuerpo(){
+    CuadroAlineaTuCuerpo()
+}
