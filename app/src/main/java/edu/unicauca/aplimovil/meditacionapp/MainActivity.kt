f95cbe9c0d25b3aabd79b2279e28916921e1d622
cfigmart <cfigmart@unicauca.edu.co>
2025-03-12 18:52:44 -0500

    Added two composables: TarjetaColeccionesFavoritas y CuadroAlineaTuCuerpo

--- app/src/main/java/edu/unicauca/aplimovil/meditacionapp/MainActivity.kt
+++ app/src/main/java/edu/unicauca/aplimovil/meditacionapp/MainActivity.kt
@@ -4,24 +4,35 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFromBaseline
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -73,14 +84,26 @@ fun PreviewBarraDeBusqueda() {
 }
 
 @Composable
-fun CuadroAlineaTuCuerpo(){
-    Column {
+fun CuadroAlineaTuCuerpo(
+    @DrawableRes imgId: Int,
+    @StringRes descripcionId: Int,
+    modifier: Modifier = Modifier,
+){
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
         Image(
-            painter = painterResource(R.drawable.ab1_inversions),
-            contentDescription = stringResource(R.string.ab1_inversions_txt)
+            painter = painterResource(imgId),
+            contentDescription = stringResource(descripcionId),
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(88.dp)
+                .clip(CircleShape)
         )
         Text(
-            text = stringResource(R.string.ab1_inversions_txt)
+            text = stringResource(descripcionId),
+            modifier = Modifier.paddingFromBaseline(top = 24.dp, bottom = 8.dp),
+            style = MaterialTheme.typography.bodyMedium
         )
     }
 }
@@ -88,5 +111,49 @@ fun CuadroAlineaTuCuerpo(){
 @Preview
 @Composable
 fun PreviewCuadroAlineaTuCuerpo(){
-    CuadroAlineaTuCuerpo()
+    CuadroAlineaTuCuerpo(
+        imgId = R.drawable.ab3_stretching,
+        descripcionId = R.string.stretching_txt,
+        modifier = Modifier.padding(8.dp)
+    )
+}
+
+@Composable
+fun TarjetaColeccionesFavoritas(
+    @DrawableRes imgId: Int,
+    @StringRes descripcionId: Int,
+    modifier: Modifier = Modifier,
+) {
+    Surface(
+        shape = MaterialTheme.shapes.medium,
+        modifier = modifier
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.width(255.dp)
+        ) {
+            Image(
+                painter = painterResource(imgId),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.size(80.dp)
+            )
+            Text(
+                text = stringResource(descripcionId),
+                style = MaterialTheme.typography.titleMedium,
+                modifier = Modifier.padding(horizontal = 16.dp)
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewTarjetaColeccionesFavoritas(){
+    TarjetaColeccionesFavoritas(
+        imgId = R.drawable.fc1_short_mantras,
+        descripcionId = R.string.short_mantras_txt,
+        modifier = Modifier.padding(8.dp)
+
+    )
 }
